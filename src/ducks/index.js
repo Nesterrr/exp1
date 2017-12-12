54b61a6b72827b3294b502759116ef6925ce8627
nesterrr <2626246q@gmail.com>
2017-12-12 20:45:42 +0300

    pagination

--- src/ducks/index.js
+++ src/ducks/index.js
@@ -1,26 +1,28 @@
 import { takeEvery } from 'redux-saga';
-import {call, put} from 'redux-saga/effects'
+import {call, put, select} from 'redux-saga/effects'
 
-//const API_KEY = 'api-key=3fedd9b92cac4aa398911c2dc1350724';
 
 const URL = 'http://api.nytimes.com/svc/movies/v2/reviews/search.json?critics-pick=Y&order=by-date&offset=0&api-key=3fedd9b92cac4aa398911c2dc1350724';
 function setUrl(offset) {
-    const basicURL = 'http://api.nytimes.com/svc/movies/v2/reviews/search.json?critics-pick=Y&order=by-date&';
+    const basicURL = 'http://api.nytimes.com/svc/movies/v2/reviews/search.json?critics-pick=Y&order=by-date&offset=';
     const APIkey = '&api-key=3fedd9b92cac4aa398911c2dc1350724';
 
     return basicURL + offset + APIkey;
 }
-function getData() {
+export const getProject = (state) => state.next;
+
+function getData(a) {
+    console.log('get', a);
     return new Promise((resolve) => {
-        resolve(fetch(URL).then(function(response) {
+        resolve(fetch(setUrl(a)).then(function(response) {
             return response.json();
         }).then((res) => {
             return res;
         }));
     });
 }
-function* loadData() {
-    const data = yield call(getData);
+function* loadData(a) {
+    const data = yield call(getData,a);
 
     yield put({type: 'TO_STORE',data: data});
 
@@ -28,10 +30,9 @@ function* loadData() {
 
 
 function* nextLoad() {
-   // const resultURL = basicURL + 'offset=' + offset + APIkey;
-    console.log(this);
-    yield loadData();
-    // вызывать loadData
+    const a = yield select(getProject);
+    yield console.log('111 ',a);
+    yield loadData(a);
 }
 function* rootSaga() {
     yield [
